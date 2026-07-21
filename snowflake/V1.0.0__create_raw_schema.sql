1a8bc30975e71dc23d720f67618d29be1cd5ecec
Peter Pham <peter.pham@snowflake.com>
2026-07-20 17:35:03 -0700

    Add deploy tool comment to trigger pipeline

--- snowflake/V1.0.0__create_raw_schema.sql
+++ snowflake/V1.0.0__create_raw_schema.sql
@@ -1,5 +1,6 @@
 -- Migration: V1.0.0
 -- Description: Create the RAW schema for landing data
 -- Author: Peter Pham
+-- Deployed via: schemachange + GitHub Actions
 
 CREATE SCHEMA IF NOT EXISTS RAW;
